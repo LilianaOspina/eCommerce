8527b9e5b12974debdc29b0c05f5e3aa1fcfe66e
Liliana <liliana.ospina.u@gmail.com>
2020-10-14 14:23:46 -0300

    Navbar changes

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,25 +1,26 @@
 import React from "react";
 import "../../App.scss";
 
-function Field({link,text1, text2, text3 }){
-    return  <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="http://www.google.com.ar" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    {link}
+function Field({ link, text1, text2, text3 }) {
+    return (
+        <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="http://www.google.com.ar" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                {link}
+            </a>
+            <div className="nb-menu dropdown-menu" aria-labelledby="dropdown01">
+                <a className="nb-list dropdown-item" href="http://www.google.com.ar">
+                    {text1}
                 </a>
-                <div className="nb-menu dropdown-menu" aria-labelledby="dropdown01">
-                    <a className="nb-list dropdown-item" href="http://www.google.com.ar">
-                        {text1}
-                    </a>
-                    <a className="nb-list dropdown-item" href="http://www.google.com.ar">
-                        {text2}
-                    </a>
-                    <a className="nb-list dropdown-item" href="http://www.google.com.ar">
-                        {text3}
-                    </a>
-                </div>
-            </li>
+                <a className="nb-list dropdown-item" href="http://www.google.com.ar">
+                    {text2}
+                </a>
+                <a className="nb-list dropdown-item" href="http://www.google.com.ar">
+                    {text3}
+                </a>
+            </div>
+        </li>
+    );
 }
-
 
 function Navbar() {
     return (
@@ -29,21 +30,12 @@ function Navbar() {
             </a>
             <div className="nb-links navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
                 <ul className="navbar-nav mr-auto">
-                    
-                    <Field link="New In" text1="bras" text2="Panties" text3="linguerie">
-                    </Field>
-                    <Field link="Collection" text1="SpringTime" text2="SummerTops" text3="LaceAge">
-                    </Field>
-                    <Field link="Sale" text1="2019 Temp" text2="2018 Temp" text3="Past years...">
-                    </Field>
-
+                    <Field link="New In" text1="Bras" text2="Panties" text3="Linguerie"></Field>
+                    <Field link="Collection" text1="SpringTime" text2="SummerTops" text3="LaceAge"></Field>
+                    <Field link="Sale" text1="2019 Temp" text2="2018 Temp" text3="Past years..."></Field>
                 </ul>
                 <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link" href="http://www.google.com.ar">
-                            Contact
-                        </a>
-                    </li>
+                    <Field link="Contact" text1="E-mail" text2="What's App"></Field>
                     <li className="nav-item active">
                         <a className="nav-link" href="http://www.google.com.ar">
                             <i class="fas fa-shopping-cart"></i>
@@ -54,7 +46,7 @@ function Navbar() {
                 <form className="form-inline my-2 my-lg-0">
                     <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
                     <button className="btn btn-outline-info my-2 my-sm-0 btn-style" type="submit">
-                        Buscar
+                        buscar
                     </button>
                 </form>
             </div>
